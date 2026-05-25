3e3a7aaeef43b67b9a783a7476eae2ca889ad94f
Hamid Raza <hamidrazabajwa49@gmail.com>
2026-05-25 16:15:41 +0500

    Implement BetaBinomial class with update and interval methods
    Added BetaBinomial class for Bayesian analysis.

--- Bayesian/conjugate.py
+++ Bayesian/conjugate.py
@@ -8,3 +8,36 @@ if parent_dir not in sys.path:
     sys.path.insert(0, parent_dir)
 
 from Probability.continuous import NormalDistribution,BetaDistribution
+
+class BetaBinomial:
+    
+    def __init__(self, alpha_prior, beta_prior):
+        self.alpha = alpha_prior
+        self.beta = beta_prior
+        self.prior = BetaDistribution(alpha_prior, beta_prior)
+
+    def update(self, n_trials: int, k_success: int):
+        self.alpha += k_success
+        self.beta += (n_trials - k_success)
+        self.posterior = BetaDistribution(self.alpha, self.beta)
+
+    def posterior_mean(self):
+        return self.posterior.mean()
+
+    def credible_interval(self, prob=0.95):
+        # Get z from standard normal using your ppf
+        norm = NormalDistribution(0.0, 1.0)
+        z = norm.ppf((1.0 + prob) / 2.0)
+
+        mu = self.posterior.mean()
+        std = math.sqrt(self.posterior.variance())
+        lower = mu - z * std
+        upper = mu + z * std
+        
+        return {
+            "prob": prob,
+            "lower": lower,
+            "upper": upper,
+            "mean": mu,
+            "std": std
+        }
