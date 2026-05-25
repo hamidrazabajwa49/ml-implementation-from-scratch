2ac9239406039d854c85d5808957f1bc018a2235
Hamid Raza <hamidrazabajwa49@gmail.com>
2026-05-25 16:14:42 +0500

    Starting Bayesian Statistics

---
+++ Bayesian/conjugate.py
@@ -0,0 +1,10 @@
+import math
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
+
+from Probability.continuous import NormalDistribution,BetaDistribution
