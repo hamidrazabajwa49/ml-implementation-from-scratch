8902ed2888c1408f329f66bca266f4fdf8f7ce1c
Hamid Raza <hamidrazabajwa49@gmail.com>
2026-05-25 17:10:38 +0500

    Implement GammaPoisson class with credible interval
    Added GammaPoisson class for gamma prior in Poisson model.

--- Bayesian/conjugate.py
+++ Bayesian/conjugate.py
@@ -7,7 +7,7 @@ parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
 if parent_dir not in sys.path:
     sys.path.insert(0, parent_dir)
 
-from Probability.continuous import NormalDistribution,BetaDistribution
+from Probability.continuous import NormalDistribution,BetaDistribution,GammaDistribution
 
 class BetaBinomial:
     
@@ -41,3 +41,36 @@ class BetaBinomial:
             "mean": mu,
             "std": std
         }
+
+class GammaPoisson:
+
+    def __init__(self,alpha_prior,beta_prior):
+        self.alpha=alpha_prior
+        self.beta=beta_prior
+        self.prior=GammaDistribution(alpha_prior,beta_prior)
+
+    def update(self,k_events,t_exposure=1.0):
+        self.alpha+=k_events
+        self.beta+=t_exposure
+        self.posterior=GammaDistribution(self.alpha,self.beta)
+
+    def posterior_mean(self):
+        return self.posterior.mean()
+
+    def credible_interval(self,prob=0.95):
+        norm = NormalDistribution(0.0, 1.0)
+        z = norm.ppf((1.0 + prob) / 2.0)
+        mu = self.posterior.mean()
+        std = math.sqrt(self.posterior.variance())
+        lower = mu - z * std
+        upper = mu + z * std
+        
+        return {
+            "prob": prob,
+            "lower": lower,
+            "upper": upper,
+            "mean": mu,
+            "std": std
+        }
+
+
