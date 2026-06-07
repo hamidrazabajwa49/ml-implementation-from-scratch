5d704ec22e69f2629a09ecf2a7d72eb8fed1fc05
Hamid Raza <hamidrazabajwa49@gmail.com>
2026-06-07 07:59:21 +0500

    Refactor optimization methods for clarity and consistency

--- Optimization/first_order.py
+++ Optimization/first_order.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import math
-from base import Optimizer
 from typing import List, Union
 
 
@@ -10,8 +9,39 @@ parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
 if parent_dir not in sys.path:
     sys.path.insert(0, parent_dir)
 
+from Optimization.base import Optimizer
 from Vectors.vector import Vector
 from Matrix.matrix import Matrix
+
+
+def _apply_update(param: Union[Vector, Matrix, float],
+                update: Union[Vector, Matrix, float]) -> Union[Vector, Matrix, float]:
+    """Return param - update as a new object of the same type."""
+    return param - update
+
+
+def _zeros_like_components(p: Union[Vector, Matrix, float]) -> list:
+    if isinstance(p, Vector):
+        return [0.0] * len(p)
+    if isinstance(p, Matrix):
+        return [[0.0] * p.n_cols for _ in range(p.n_rows)]
+    return 0.0
+
+
+def _vec_op(a: Union[Vector, float], fn) -> Union[Vector, float]:
+    if isinstance(a, Vector):
+        return Vector([fn(a[i]) for i in range(len(a))])
+    return fn(a)
+
+
+def _vec_op2(a: Union[Vector, float], b: Union[Vector, float], fn) -> Union[Vector, float]:
+    if isinstance(a, Vector) and isinstance(b, Vector):
+        return Vector([fn(a[i], b[i]) for i in range(len(a))])
+    return fn(a, b)
+
+
+def _safe_sqrt(x: float) -> float:
+    return math.sqrt(max(x, 0.0))
 
 
 class GradientDescent(Optimizer):
@@ -23,7 +53,7 @@ class GradientDescent(Optimizer):
         if len(params) != len(grads):
             raise ValueError("params and grads must have the same length")
         for i, (p, g) in enumerate(zip(params, grads)):
-            params[i] = p - self.lr * g
+            params[i] = _apply_update(p, self.lr * g)
         self.iterations += 1
 
 
@@ -38,21 +68,23 @@ class SGD(Optimizer):
         self.decay = decay
         self._velocity = None
 
-    def step(self, params: List[Union[float, Vector, Matrix]],grads: List[Union[float, Vector, Matrix]]) -> None:
+    def step(self, params: List[Union[float, Vector, Matrix]],
+            grads: List[Union[float, Vector, Matrix]]) -> None:
         if len(params) != len(grads):
             raise ValueError("params and grads must have the same length")
 
-        lr_eff = self.lr / (1.0 + self.decay * self.iterations) if self.decay > 0 else self.lr
+        lr_eff = self.lr / (1.0 + self.decay * self.iterations) if self.decay > 0.0 else self.lr
 
         if self._velocity is None:
             self._velocity = [self._zeros_like(p) for p in params]
 
         for i, (p, g) in enumerate(zip(params, grads)):
-            if self.momentum > 0:
-                self._velocity[i] = self.momentum * self._velocity[i] + g
-                params[i] = p - lr_eff * self._velocity[i]
+            if self.momentum > 0.0:
+                # standard: v = momentum*v + lr*g  ;  p = p - v
+                self._velocity[i] = self.momentum * self._velocity[i] + lr_eff * g
+                params[i] = _apply_update(p, self._velocity[i])
             else:
-                params[i] = p - lr_eff * g
+                params[i] = _apply_update(p, lr_eff * g)
 
         self.iterations += 1
 
@@ -72,7 +104,8 @@ class Momentum(Optimizer):
         self.beta = beta
         self._velocity = None
 
-    def step(self, params: List[Union[float, Vector, Matrix]],grads: List[Union[float, Vector, Matrix]]) -> None:
+    def step(self, params: List[Union[float, Vector, Matrix]],
+            grads: List[Union[float, Vector, Matrix]]) -> None:
         if len(params) != len(grads):
             raise ValueError("params and grads must have the same length")
         if self._velocity is None:
@@ -80,7 +113,7 @@ class Momentum(Optimizer):
 
         for i, (p, g) in enumerate(zip(params, grads)):
             self._velocity[i] = self.beta * self._velocity[i] + (1.0 - self.beta) * g
-            params[i] = p - self.lr * self._velocity[i]
+            params[i] = _apply_update(p, self.lr * self._velocity[i])
 
         self.iterations += 1
 
@@ -97,27 +130,25 @@ class RMSProp(Optimizer):
         super().__init__(lr)
         if not (0.0 <= beta < 1.0):
             raise ValueError("beta must be in [0, 1)")
-        if epsilon <= 0:
+        if epsilon <= 0.0:
             raise ValueError("epsilon must be positive")
         self.beta = beta
         self.epsilon = epsilon
         self._cache = None
 
-    def step(self, params: List[Union[float, Vector, Matrix]],grads: List[Union[float, Vector, Matrix]]) -> None:
+    def step(self, params: List[Union[float, Vector, Matrix]],
+            grads: List[Union[float, Vector, Matrix]]) -> None:
         if len(params) != len(grads):
             raise ValueError("params and grads must have the same length")
         if self._cache is None:
             self._cache = [self._zeros_like(p) for p in params]
 
         for i, (p, g) in enumerate(zip(params, grads)):
-            g_sq = g.element_wise(lambda x: x * x) if hasattr(g, 'element_wise') else (g * g)
+            g_sq = _vec_op(g, lambda x: x * x)
             self._cache[i] = self.beta * self._cache[i] + (1.0 - self.beta) * g_sq
-
-            denom = self._cache[i].element_wise(lambda x: math.sqrt(x) + self.epsilon) if hasattr(self._cache[i], 'element_wise') else (math.sqrt(self._cache[i]) + self.epsilon)
-
-            update = g.element_wise_with(denom, lambda a, b: a / b) * self.lr if hasattr(g, 'element_wise_with') else (g / denom * self.lr)
-
-            params[i] = p - update
+            denom = _vec_op(self._cache[i], lambda x: _safe_sqrt(x) + self.epsilon)
+            update = self.lr * _vec_op2(g, denom, lambda a, b: a / b)
+            params[i] = _apply_update(p, update)
 
         self.iterations += 1
 
@@ -130,11 +161,12 @@ class RMSProp(Optimizer):
 
 
 class Adam(Optimizer):
-    def __init__(self, lr: float = 0.001, beta1: float = 0.9, beta2: float = 0.999, epsilon: float = 1e-8):
+    def __init__(self, lr: float = 0.001, beta1: float = 0.9, beta2: float = 0.999,
+                epsilon: float = 1e-8):
         super().__init__(lr)
         if not (0.0 <= beta1 < 1.0) or not (0.0 <= beta2 < 1.0):
             raise ValueError("betas must be in [0, 1)")
-        if epsilon <= 0:
+        if epsilon <= 0.0:
             raise ValueError("epsilon must be positive")
         self.beta1 = beta1
         self.beta2 = beta2
@@ -142,7 +174,8 @@ class Adam(Optimizer):
         self._m = None
         self._v = None
 
-    def step(self, params: List[Union[float, Vector, Matrix]],grads: List[Union[float, Vector, Matrix]]) -> None:
+    def step(self, params: List[Union[float, Vector, Matrix]],
+            grads: List[Union[float, Vector, Matrix]]) -> None:
         if len(params) != len(grads):
             raise ValueError("params and grads must have the same length")
         if self._m is None:
@@ -151,20 +184,16 @@ class Adam(Optimizer):
 
         self.iterations += 1
         t = self.iterations
+        # alpha_t already encodes bias correction
         alpha_t = self.lr * math.sqrt(1.0 - self.beta2 ** t) / (1.0 - self.beta1 ** t)
 
         for i, (p, g) in enumerate(zip(params, grads)):
             self._m[i] = self.beta1 * self._m[i] + (1.0 - self.beta1) * g
-            g_sq = g.element_wise(lambda x: x * x) if hasattr(g, 'element_wise') else (g * g)
+            g_sq = _vec_op(g, lambda x: x * x)
             self._v[i] = self.beta2 * self._v[i] + (1.0 - self.beta2) * g_sq
-
-            m_hat = self._m[i] / (1.0 - self.beta1 ** t)
-            v_hat = self._v[i] / (1.0 - self.beta2 ** t)
-
-            denom = v_hat.element_wise(lambda x: math.sqrt(x) + self.epsilon) if hasattr(v_hat, 'element_wise') else (math.sqrt(v_hat) + self.epsilon)
-            update = m_hat.element_wise_with(denom, lambda a, b: a / b) * alpha_t if hasattr(m_hat, 'element_wise_with') else (m_hat / denom * alpha_t)
-
-            params[i] = p - update
+            denom = _vec_op(self._v[i], lambda x: _safe_sqrt(x) + self.epsilon)
+            update = alpha_t * _vec_op2(self._m[i], denom, lambda a, b: a / b)
+            params[i] = _apply_update(p, update)
 
     def reset(self):
         super().reset()
@@ -172,6 +201,5 @@ class Adam(Optimizer):
         self._v = None
 
     def get_config(self):
-        return {**super().get_config(), "beta1": self.beta1, "beta2": self.beta2, "epsilon": self.epsilon}
-
-  
+        return {**super().get_config(), "beta1": self.beta1, "beta2": self.beta2,
+                "epsilon": self.epsilon}
