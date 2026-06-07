a0c880366620ab15f460d602a4b8dcba2a38b38a
Hamid Raza <hamidrazabajwa49@gmail.com>
2026-06-07 07:35:54 +0500

    Refactor code for exceptional handling

--- Probability/hypothesis_tests.py
+++ Probability/hypothesis_tests.py
@@ -1,6 +1,14 @@
 import math
-from descriptive_stats import DescriptiveStats
-from continuous import TDistribution, Chi2Distribution, FDistribution
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
+
+from Probability.descriptive_stats import DescriptiveStats
+from Probability.continuous import TDistribution, Chi2Distribution, FDistribution
 
 
 def ttest_1samp(data: list,pop_mean: float,alternative: str = "two-sided",alpha: float = 0.05,) -> dict:
